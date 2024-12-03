e90785583586f89193e9594a13f5a42079a652e5
arjun3649 <arjun.rakwal3649@gmail.com>
2024-12-04 02:28:33 +0530

    solved deployment errors

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useUser } from '@clerk/clerk-react';
+import { SignInButton } from '@clerk/nextjs';
 
 const HomePage = () => {
+  const { isSignedIn } = useUser();
   return (
     <div className="flex flex-col justify-start min-h-screen md:justify-center items-center h-full bg-gradient-to-r from-blue-500 to-purple-500  pt-20 md:pt-0 ">
       <div className="w-11/12 max-w-md mx-auto text-center space-y-4 p-6 md:p-8 rounded-lg bg-white shadow-xl shadow-black hover:shadow-gray-700">
@@ -8,9 +11,12 @@ const HomePage = () => {
         <p className="text-base md:text-lg text-gray-600">
           Explore our features and get access to the dashboard.
         </p>
-        <Link href="/dashboard" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md transition-colors duration-300">
+        {isSignedIn ? <Link href="/dashboard" className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md transition-colors duration-300">
           Go to Dashboard
-        </Link>
+        </Link>:<div className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md transition-colors duration-300">
+        <SignInButton forceRedirectUrl="/dashboard" mode="modal" >  Go to Dashboard </SignInButton>
+        </div>}
+        
       </div>
     </div>
   );
